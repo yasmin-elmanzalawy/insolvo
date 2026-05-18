724387c9756ab0febe249a0e0143d7ff1fbec853
Yasmin Elmanzalawy <yasminelmanzalawy@Yasmins-MacBook-Air.local>
2026-05-18 21:06:38 +0300

    developer's whatsapp

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,11 +1,19 @@
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-200 bg-[#10254c]">
-      <div className="mx-auto max-w-6xl px-6 py-3">
-        <p className="text-sm text-white">
-          © 2026 Created by Yasmin Elmanzalawy
-        </p>
-      </div>
-    </footer>
+   <footer className="border-t border-gray-200 bg-[#10254c]">
+  <div className="mx-auto max-w-6xl px-6 py-3">
+    <p className="text-sm text-white">
+      © 2026 Created by{" "}
+      <a
+        href="https://wa.me/201094837186"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="font-bold text-[#CBFFFE] hover:text-gray-300 transition "
+      >
+        Yasmin Elmanzalawy
+      </a>
+    </p>
+  </div>
+</footer>
   );
 }
